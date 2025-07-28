e90e12c13014af8ed46009915ba04475fd9d1cac
André Montaño <andrefernando2000@gmail.com>
2025-07-27 20:07:59 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container mx-auto mt-20 ">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de Pacientes {''}
+          <span className="text-indigo-700">Veterinarios</span>
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
