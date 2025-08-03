8272e6bd56246267fe30ca616773aa7733f54e3c
André Montaño <andrefernando2000@gmail.com>
2025-08-02 23:49:23 -0400

    Correcciones de texto

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -28,7 +28,7 @@ export default function PatientsList() {
             <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
             <p className="text-xl mt-5 mb-10 text-center">
               Comienza agregando pacientes y {''}
-              <span className="text-indigo-600 font-bold">apareceran aquí</span>
+              <span className="text-indigo-600 font-bold">aparecerán aquí</span>
             </p>
           </>
       )}
